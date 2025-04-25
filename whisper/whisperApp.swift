b2aaea1acce3de1f77ab5d00e920e7093eddbf39
EthanMCao <ethanmcao@gmail.com>
2025-04-25 11:19:04 -0700

    Initial Commit

---
+++ whisper/whisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisperApp.swift
+//  whisper
+//
+//  Created by Ethan Cao on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct whisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
